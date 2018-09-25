b6d945c380a869e75f671bd0977212574bd4de6c
Sara <sara@Sara>
2018-09-25 18:23:19 +0200

    Add basi.py, strutture.py

---
+++ basi.py
@@ -0,0 +1,95 @@
+type(4) #funziona solo su riga di comando, serve per sapere il tipo della variabile tra parentesi
+print type(4) #stampa il tipo da un file di script
+
+#print type(ciao) #non funziona perche non e' racchiuso tra virgolette
+
+#i nomi dati alle variabili non devono cominciare per numero, contenere caratteri non validi (ex. $) e non devono appartenere alle parole riservate
+a = 12 #dichiarazione 
+print a #stampa
+
+x = 3**2 #elevamento a potenza 
+
+messaggio = "ciao"
+print messaggio #stampa il valore della varabile 
+messaggio #riga di comando stampa il valore della variabile tra virgolette
+#per stampare le virgolette uso print ' "messaggio" ', viceversa print " 'messaggio' "
+
+#operazioni sulle stringhe
+mex = "oh " #spazio all'interno
+print mex + messaggio #concatenamento
+print mex * 3 #ripetizione
+
+print mex, a #la virgola spazia
+print messaggio, a #non funziona senza virgola
+
+#funzioni
+print id(a) #identificatore nella memoria 
+type(a) #tipo della variabile
+int(a) #conversione in intero (troncamento della parte decimale)
+float() #conversione di interi e stringhe to float
+str() #conversione in stringhe
+# 1 e 1.0 sono numeri differenti per tipo
+#conversione forzata (intero) / (float) = (float)
+pow(3, 5) #base esponente
+
+import math #modulo matematico
+#funzioni: log10, sin, cos, pi, sqrt, log, exp
+print math.log10(2) #chiamata identificata col punto
+
+#nuove funzioni
+def nuova_funzione(): #definizione della funzione
+	print "ciao" , messaggio
+#identazione obbligatoria -> { }
+
+print nuova_funzione #stampa il nome e l'area di memoria
+nuova_funzione() #parentesi per richiamare la funzione
+#la definizione precede la chiamata
+
+#booleani
+# == uguale, != diverso, >=, < etc.
+print 5 == 5
+5 == 5 #su riga di comando stampa 1
+#and or not
+#in controlla se un valore e' presente in una variabile
+
+#IF
+a = -35
+if a > 2:
+	print "y"
+elif a < 0: #else if (condizione)
+	 print "n"	
+else: 
+	print "a"
+
+#inserimento -> input
+#funzione raw input, funziona con tutti i tipi di input
+ins = raw_input("scrivi qualcosa \t") #\n per andare a capo, \t per tab
+print ins
+
+#funzione input, funziona solo con numeri
+ins = "anni? "
+anni = input(ins) 
+
+def ValAss(x):
+	if x > 0 : 
+		return x
+	elif x < 0 : 
+		return -x
+#nel caso in cui sia zero viene stampato None poiche non si hanno istruzioni 
+
+x = input("inserisci un numero ")
+print ValAss(x)
+
+#while
+while x>0 :
+	print x
+	x -= 1 #x-- non funziona
+
+i = 1
+while i < 5 :
+	print i, '	' , #senza virgola va a capo ad ogni iterazione 
+	i += 1
+print #va a capo dopo aver finito il ciclo
+
+
+
---
+++ strutture.py
@@ -0,0 +1,135 @@
+#stringhe (array di char) [immutabili]
+frutto = "banana"
+lettera = frutto[3]
+print lettera
+#come le liste anche le stringhe sono enumerate da 0
+print frutto[0]
+
+print len(frutto) #len restituisce la lunghezza della stringa
+print frutto[-2] #stampa il penultimo carattere
+#con i numeri negativi si stampa dalla fine
+
+#iterazione della stringa con ciclo while
+indice = 0
+while indice < len(frutto):
+	print frutto[indice]
+	indice += 1
+
+#iterazione della stringa con for
+for lettera in frutto:
+	print lettera	
+
+#porzioni di stringa
+print frutto[:7] 
+#[num:] prende dall'indice num compreso, fino alla fine
+#[:num] prende dall'inizio a indice num escluso
+#[n:m] da n incluso a m escluso
+#[:] tutto
+#se num eccede verra' stampato fino all'ultimo carattere
+
+#posso confrontare le stringhe con gli operatori booleani per mettere le stringhe in ordine, ricordando che le lettere maiuscole vengono prima di quelle minuscole
+
+#le stringhe sono immutabili, non posso fare frutto[3] = 'f'
+
+import string #modulo che contiene funzioni per le stringhe
+
+posizione = string.find(frutto, 'a') #cerco nella variabile del primo argomento dove compare la lettera specificata nel secondo argomento
+#funziona anche per cercare sottostringhe
+string.find(frutto, "an")
+#(variabile, stringa che cerco, indice da dove cominciare(incluso), indice massimo(escluso))
+
+#string: find, lowercase, uppercase,digits, whitespace, split, join
+#split e join convergono stringhe da/a liste
+
+
+#alias
+cibo = "banana" #entrambe le variabili puntano allo stesso oggetto
+print frutto, id(frutto)
+print cibo, id(cibo)
+
+
+
+#liste (array di tipo) [mutabili]
+
+#creazione
+lista = [1, 2, 'a', "asd", [2,4]]
+lista = range(1,5) #crea una lista con elementi da 1(incluso) a 5(escluso)
+lista = range(5) #crea una lista da 0 incluso a 5 escluso
+lista = range(1, 10, 3) #crea una lista  da 1 a 10 escluso ogni 3 elementi
+lista = [] #lista vuota
+#print lista
+
+lista = range(5)
+print lista
+lista[2] = 'a'
+lista[5-4] = 'b'
+print lista
+#lista[10] da errore
+print lista[-3]
+len(lista)
+
+for var in lista:
+	print var #var = lista[index]
+
+#l'operatore + concatena le liste
+#l'operatore * ripete una lista n volte
+#[:] prende tutta la lista
+#[n:], [:n], [n:m]
+
+#e' possibile aggiornare le liste tramite assegnazione
+lista[2] = 'a'
+#e' possibile modificare le liste con assegnazione di porzione
+lista[3:] = [22,24,25]
+print lista
+#e' possibile rimuovere uno o piu elementi assegnando la lista vuota
+lista[2:5] = []
+print lista
+#e' possibile inserire elementi nel mezzo della lista, prima o dopo
+lista[1:1] = [1.0,3.5,5.6]
+print lista
+
+#cancellazione delle liste
+del lista[:] 
+print lista
+
+#alias
+lista = range(3)
+lista2 = range(3) #si crea un altro puntatore con oggetto diverso
+print lista, id(lista)
+print lista2, id(lista2)
+
+#clonazione
+a = range(2)
+b = a[:] #a[:] crea il clone, a soltanto crea una copia(id uguali)
+print a, b, id(a), id(b)
+
+#ogni lista passata ad una funzione viene passata per riferimento 
+
+
+lista = [1, 2, 'a', "asd", [2,4]]
+#per accedere alla sottolista uso
+print lista[4][0] #[indice lista][indice sottolista]
+
+#matrici di righe con le liste
+matrix = [[2,3], [5,6]] #[prima riga] [seconda riga]
+print matrix[0], "\n", matrix[1]
+#accedo alla matrice con matrix[riga][colonna]
+print matrix[0][1]
+
+
+
+
+
+#tuple [immutabili]
+
+#dizionari [mutabili]
+
+
+
+
+
+
+
+
+
+
